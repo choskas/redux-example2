ed2ae42f6a6a5b81d8b95812441cad03978191d5
Choskas Delta <Choskas Delta>
2019-11-10 20:45:20 -0600

    spreading objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,37 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid'
+//ADD_EXPENSE
+const addExpense = ({description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description: '',
+    note: '',
+    amount: 0,
+    createdAt : 0
 
+  }
+})
+
+//Remove expense
+
+const removeExpense = ({ id }) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+})
+
+//EDIT_EXPENSE 
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+})
+
+//Change text
+const setTextFilter = (text= '') => ({
+  type: 'CANGE_TEXT',
+  text
+})
 
 //Expenses Reducer
 
@@ -7,10 +39,30 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) =>{
   switch(action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, 
+              action.expense
+      ]
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id}) => id !== action.id) 
     default:
       return state
+    case 'EDIT_EXPENSE':
+      return state.map((expense)=>{
+        if(expense.id === action.id){
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense
+        }
+      })  
+  
   }
 }
+
+
 
 //Filters Reducer
 
@@ -23,6 +75,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) =>{
   switch(action.type) {
+    case 'SET_TEXT_FILTER':
+        return {
+          ...state,
+          text: action.text
+        }
     default:
       return state
   }
@@ -37,9 +94,19 @@ const store = createStore(combineReducers(
   
   ))
 
+  store.subscribe(()=>{
+    console.log(store.getState()) 
+  })
+
+  const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}))
+
+
+  store.dispatch(removeExpense({id: expenseOne.expense.id}))
+  store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}))
+  console.log(expenseOne)
 // const store = createStore(expensesReducer)
 
-console.log(store.getState()) 
+
 
 const demoState = {
   expenses: [{
@@ -57,3 +124,9 @@ const demoState = {
   }
 };
 
+const user = {
+  name: 'PUTO',
+  age: 24
+}
+
+console.log({...user, location: "mi casa"})
